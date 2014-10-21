9b19029f462faca3fe34a28f56c5258d3b7f6170
Teemu Laukka <teemulaukka@gmail.com>
2014-10-21 14:08:18 +0300

    Add multi guest token support.

--- app/scripts/modules/authentication/services/authentication.js
+++ app/scripts/modules/authentication/services/authentication.js
@@ -4,8 +4,30 @@
 module.exports = function($q, $http, Config) {
 
 	var _api = Config.api.url();
+	var _tokenKey = 'tb-access-token-user';
 
 	return {
+		loginUser: function(user) {
+			var self = this;
+
+			return $http.post(_api + 'auth/login', user)
+				.then(function(response) {
+					var token = response.headers('x-access-token');
+					self.setUserToken(token);
+					// self.setTokenKey('tb-access-token-user');
+				});
+		},
+
+		loginGuest: function(boardId, accessCode, username) {
+			var self = this;
+
+			return $http.post(_api + 'boards/' + boardId + '/access/' + accessCode, { username: username })
+				.then(function(response) {
+					var token = response.headers('x-access-token')
+					self.setGuestToken(boardId, token);
+					// self.setTokenKey('tb-access-token-guest-' + boardId);
+				});
+		},
 
 		login: function(user) {
 			return $http.post(_api + 'auth/login', user)
@@ -22,6 +44,17 @@ module.exports = function($q, $http, Config) {
 				});
 		},
 
+		logoutUser: function() {
+			return $http.post(_api + 'auth/logout')
+				.then(function() {
+					removeUserToken();
+				});
+		},
+
+		logoutGuest: function(boardId) {
+			removeGuestToken(boardId);
+		},
+
 		register: function(user) {
 			return $http.post(_api + 'auth/register', user);
 		},
@@ -32,12 +65,45 @@ module.exports = function($q, $http, Config) {
 			});
 		},
 
+		getTokenKey: function() {
+			return _tokenKey;
+		},
+
+		setTokenKey: function(tokenKey) {
+			_tokenKey = tokenKey;
+		},
+
 		getToken: function() {
-			return localStorage.getItem('access-token');
+			// return localStorage.getItem('access-token');
+			return localStorage.getItem(_tokenKey);
+		},
+
+		getUserToken: function() {
+			return localStorage.getItem('tb-access-token-user');
+		},
+
+		getGuestToken: function(boardId) {
+			return localStorage.getItem('tb-access-token-guest-' + boardId);
 		},
 
 		setToken: function(token) {
 			localStorage.setItem('access-token', token);
+		},
+
+		setUserToken: function(token) {
+			localStorage.setItem('tb-access-token-user', token);
+		},
+
+		setGuestToken: function(boardId, token) {
+			localStorage.setItem('tb-access-token-guest-' + boardId, token);
+		},
+
+		removeUserToken: function() {
+			localStorage.removeItem('tb-access-token-user');
+		},
+
+		removeGuestToken: function(boardId) {
+			localStorage.removeItem('tb-access-token-guest-' + boardId);
 		},
 
 		clear: function() {
