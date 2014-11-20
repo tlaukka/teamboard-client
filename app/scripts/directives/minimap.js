fd2143e36a9dcd809357cc55a8d82338caf6b4ad
Teemu Laukka <teemulaukka@gmail.com>
2014-11-20 14:21:49 +0200

    Fix minimap layout.

--- app/scripts/directives/minimap.js
+++ app/scripts/directives/minimap.js
@@ -22,26 +22,24 @@ module.exports = function($window, $timeout) {
 			// Set minimap size according to scale.
 			// One horizontal unit: 240px
 			// One vertical unit: 135px
-			var unitH = 240;
-			var unitV = 135;
-			var marginTop = 100;
+			var unitW = 240;
+			var unitH = 135;
+			var marginTop = 140;
+			var marginRight = 80;
+			var marginBottom = 80;
 			var marginLeft = 80;
-			var scale = scope.width / (scope.board.size.width * unitH);
-			// var width = Math.floor(scope.board.size.width * 240 * scale) + 'px';
-			// var height = Math.floor(scope.board.size.height * 135 * scale) + 'px';
 
-
-			var width = Math.floor((scope.board.size.width * unitH + marginTop + marginLeft) * scale) + 'px';
-			var height = Math.floor((scope.board.size.height * unitV + 2 * marginLeft) * scale) + 'px';
-			var bgPos = Math.floor(marginLeft * scale) + 'px ' + Math.floor(marginTop * scale) + 'px';
-			var bgWidth = Math.floor(scope.board.size.width * unitH * scale) + 'px';
-			var bgHeight = Math.floor(scope.board.size.height * unitV * scale) + 'px';
+			var scale = scope.width / (scope.board.size.width * unitW + marginLeft + marginRight);
+			var width = ((scope.board.size.width * unitW + marginLeft + marginRight) * scale) + 'px';
+			var height = ((scope.board.size.height * unitH + marginTop + marginBottom) * scale) + 'px';
+			var bgPos = (marginLeft * scale) + 'px ' + (marginTop * scale) + 'px';
+			var bgWidth = (scope.board.size.width * unitW * scale + 2) + 'px';
+			var bgHeight = (scope.board.size.height * unitH * scale + 1) + 'px';
 			var bgSize = bgWidth + ' ' + bgHeight;
 
 			element.css({
 				'width': width,
 				'height': height,
-				// 'background-size': '100%'
 				'background-position': bgPos,
 				'background-size': bgSize
 			});
@@ -51,10 +49,6 @@ module.exports = function($window, $timeout) {
 					scope.updateIndicator();
 				}, 0);
 
-				// scope.$on('action:sidebar-collapse', function(event, isCollapsed) {
-				// 	scope.updateIndicator(isCollapsed);
-				// });
-
 				angular.element($window).bind('resize', function() {
 					scope.updateIndicator();
 				});
@@ -63,22 +57,15 @@ module.exports = function($window, $timeout) {
 			scope.setTicket = function(ticket) {
 				return {
 					'background-color': ticket.color,
-					'left': ((ticket.position.x + 80) * scale) + 'px',
-					'top': ((ticket.position.y + 100) * scale) + 'px',
+					'top': ((ticket.position.y + marginTop) * scale) + 'px',
+					'left': ((ticket.position.x + marginLeft) * scale) + 'px',
 					'width': (226 * scale) + 'px',
 					'height': (128 * scale) + 'px'
 				}
 			}
 
 			scope.updateIndicator = function(isSidebarCollapsed) {
-				// if ($window.innerWidth < 768) {
-				// 	isSidebarCollapsed = true;
-				// }
-
-				// var indicatorWidth = isSidebarCollapsed ? ($window.innerWidth - 74) : ($window.innerWidth - 232);
 				var indicatorWidth = $window.innerWidth * scale;
-				// indicatorWidth *= scale;
-				// var indicatorHeight = ($window.innerHeight - 64) * scale;
 				var indicatorHeight = $window.innerHeight * scale;
 
 				var indicator = angular.element(element.children()[0]);
