bbdf4e99863e763419f158521c0ef28635cbc740
Teemu Laukka <teemulaukka@gmail.com>
2014-11-27 15:20:43 +0200

    Fix workspace scrolling.

--- app/scripts/directives/workspace.js
+++ app/scripts/directives/workspace.js
@@ -30,6 +30,7 @@ module.exports = function(scrollArea, $timeout, $window, $document) {
 				var scroller = angular.element(document.getElementById('content-scrollarea'));
 
 				// workspaceWidth / (boardPreviewWidth + margin)
+				console.log('scroller w: ' + scroller[0].clientWidth);
 				var boardsPerRow = Math.floor(scroller[0].clientWidth / 254);
 				boardsPerRow = Math.min(Math.max(boardsPerRow, 1), 8);
 
@@ -38,9 +39,10 @@ module.exports = function(scrollArea, $timeout, $window, $document) {
 					// Partial row
 					boardRowCount++;
 				}
-
+				console.log('br: ' + boardRowCount);
 				// (boardRowCount * boardPreviewHeight + margin) + 2 * margin
-				var height = (boardRowCount * 224 + 48) + 'px';
+				var height = (boardRowCount * 212 + 48 + 140) + 'px';
+				console.log('h: ' + height);
 				element.css('height', height);
 			}
 
